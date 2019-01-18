c5241608b626aa7fa72c55914ab9046a174588ae
Marek Cichy <46812669+MarekCichy@users.noreply.github.com>
2019-01-18 12:04:36 +0100

    Create exc10_3.py

---
+++ exc10_3.py
@@ -0,0 +1,37 @@
+import string
+fname = input('Enter the file name: ')
+try:
+    fhand = open(fname, encoding='utf8')
+except:
+    print('File cannot be opened:', fname)
+    eeaxit()
+counts = dict()
+
+for line in fhand:
+    line = line.translate(str.maketrans('', '', string.punctuation))
+    line = line.lower()
+    words = line.split()
+    #print (words)
+    for x in words:
+        word = x.split()
+        word = word [0]
+        #print (word)
+        for y in range (0 , len(word)):
+            letter = word[y]
+            #print (letter)
+            if letter.isalpha() == True:
+                counts [letter] = counts.get(letter,0) + 1
+
+#print (counts)
+
+lst = list()
+for ltr, count in list(counts.items()):
+    lst.append((count, ltr))
+    
+lst.sort(reverse=True)
+
+for count, ltr in lst:
+    print(count, ltr)
+
+#sanity check : how many letters
+print (len(lst))
